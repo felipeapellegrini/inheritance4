ee3217db20ce97a1c51ab59b3079bdfcef1cd105
Felipe A Pellegrini <felipeapellegrini@gmail.com>
2019-09-27 10:34:25 -0300

    Inheritance 4, abstract class/methods.

---
+++ Inheritance4/Entities/Individual.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace Inheritance4.Entities
+{
+    class Individual : TaxPayer
+    {
+
+        public double HealthExpenditures { get; set; }
+
+        public Individual (double healthExpenditures, string name, double anualIncome) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+        public override double Tax()
+        {
+            if(HealthExpenditures > 0)
+            {
+                if (AnualIncome < 20000.00)
+                {
+                    return (AnualIncome * 0.15) - (HealthExpenditures * 0.5);
+                }
+                else
+                {
+                    return (AnualIncome * 0.25) - (HealthExpenditures * 0.5);
+                }
+            }
+            else
+            {
+                if (AnualIncome < 20000.00)
+                {
+                    return AnualIncome * 0.15;
+                }
+                else
+                {
+                    return AnualIncome * 0.25;
+                }
+            }
+        }
+    }
+}
---
+++ Inheritance4/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inheritance4.Entities;
+
+namespace Inheritance4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TaxPayer> list = new List<TaxPayer>();
+
+            Console.Write("Enter the number of Tax Payers: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Tax payer #{i} data:");
+                Console.Write("Individual or company? (i/c): ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Anual Income: ");
+                double anualIncome = double.Parse(Console.ReadLine());
+                if (ch == 'i')
+                {
+                    Console.Write("Health expenditures: ");
+                    double healthExpenditures = double.Parse(Console.ReadLine());
+
+                    list.Add(new Individual(healthExpenditures, name, anualIncome));
+                }
+                else
+                {
+                    Console.Write("Number of employees: ");
+                    int numberOfEmployees = int.Parse(Console.ReadLine());
+
+                    list.Add(new Company(name, anualIncome, numberOfEmployees));
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("TAXES PAYD");
+            double sum = 0;
+
+            foreach (TaxPayer x in list)
+            {
+                Console.WriteLine(x.Name + ": $" + x.Tax().ToString("F2"));
+                sum += x.Tax();
+            }
+
+            
+
+          
+            Console.WriteLine();
+            Console.WriteLine("TOTAL TAXES: $" + sum.ToString("F2"));
+        }
+    }
+}
